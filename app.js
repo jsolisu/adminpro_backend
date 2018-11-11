3e7c0e04af272170777a213179eb2263d0a6f35d
jsolisu <jcsolisu2000@gmail.com>
2018-11-11 14:16:18 -0600

    Ruta inicial

---
+++ app.js
@@ -0,0 +1,18 @@
+// requires
+const express = require('express');
+
+// inicializar variables
+const app = express();
+
+// rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Peticion realizada correctamente'
+    });
+});
+
+// escuchar peticiones
+app.listen(3000, () => {
+    console.log('Express server puerto 3000: \x1b[32m%s\x1b[0m', 'online.');
+});
