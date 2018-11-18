332b456dc1485fe512526cf6ab9f3c20fd82b3d8
jsolisu <jcsolisu2000@gmail.com>
2018-11-18 17:37:30 -0600

    Actualizar usuario

--- routes/usuario.js
+++ routes/usuario.js
@@ -24,6 +24,53 @@ app.get('/', (req, res, next) => {
             });
 });
 
+// Actualizar usuario
+app.put('/:id', (req, res, next) => {
+    let id = req.params.id;
+    let body = req.body;
+
+    Usuario.findById(id, (err, usuario) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        }
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'El usuario con el id ' + id + ' no existe',
+                errors: {
+                    message: 'No existe un usuario con ese ID'
+                }
+            });
+        }
+
+        usuario.nombre = body.nombre;
+        usuario.email = body.email;
+        usuario.role = body.role;
+
+        usuario.save((err, usuarioGuardado) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Error al actualizar usuario',
+                    errors: err
+                });
+            }
+
+            usuarioGuardado.password = '-';
+
+            res.status(200).json({
+                ok: true,
+                usuario: usuarioGuardado
+            });
+        });
+    });
+});
+
 // Crear usuario
 app.post('/', (req, res, next) => {
     let body = req.body;
